0eb2debbb66ce0b6a442268080afda55e5858729
Ryan Whelan <rcwhelan@gmail.com>
2015-06-07 23:07:39 -0400

    Formatting :-/

--- LidarSensor.go
+++ LidarSensor.go
@@ -3,16 +3,16 @@ package main
 var SensorAddr int = 0x62
 
 type LidarSensor struct {
-    i2cAddr     int
-    bus         *I2cBus
-//    Distance    uint16
+	i2cAddr int
+	bus     *I2cBus
+	//    Distance    uint16
 }
 
 func NewLidarSensor(i2cbus *I2cBus) (*LidarSensor, error) {
-    sensor := &LidarSensor{bus: i2cbus, i2cAddr: SensorAddr}
-    err := sensor.bus.SetAddr(sensor.i2cAddr)
-    if err != nil {
-        return nil, err
-    }
-    return sensor, nil
+	sensor := &LidarSensor{bus: i2cbus, i2cAddr: SensorAddr}
+	err := sensor.bus.SetAddr(sensor.i2cAddr)
+	if err != nil {
+		return nil, err
+	}
+	return sensor, nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 var I2CBusPath string = "/dev/i2c-1"
@@ -9,13 +9,13 @@ var I2CBusPath string = "/dev/i2c-1"
 func main() {
 	i2c, err := NewI2cBus(I2CBusPath)
 	if err != nil {
-        panic(err)
-    }
+		panic(err)
+	}
 
-    sensor, err := NewLidarSensor(i2c)
-    if err != nil {
-        panic(err)
-    }
-    
-    fmt.Println(sensor.i2cAddr)
+	sensor, err := NewLidarSensor(i2c)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(sensor.i2cAddr)
 }
